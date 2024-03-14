e58a10935c66bfe50daadc6bd7f7796e936017ca
gregorio-calabretta <132211711+gregorio-calabretta@users.noreply.github.com>
2024-03-14 09:25:55 +0100

    first commit

---
+++ src/main/java/com/example/GestioneContocorrente/GestioneContocorrenteApplication.java
@@ -0,0 +1,13 @@
+package com.example.GestioneContocorrente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioneContocorrenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioneContocorrenteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GestioneContocorrente/GestioneContocorrenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GestioneContocorrente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneContocorrenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
